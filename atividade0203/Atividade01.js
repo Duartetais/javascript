9a199c1e172217f5beaa1102b682de7eb01c2df4
Tais Duarte Bernardi <tahysduarte22@gmail.com>
2026-03-06 15:02:52 -0300

    Delete atividade0203 directory

--- atividade0203/Atividade01.js
+++
@@ -1,8 +0,0 @@
-const leia = require('readline-sync');
-
-let salario = leia.questionFloat("Digite o salário: ");
-let abono = leia.questionFloat("Digite o abono: ");
-
-let novoSalario = salario + abono;
-
-console.log("Novo salário: " + novoSalario);
