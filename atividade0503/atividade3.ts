73ca98a4655c40098632768b1d9bc5db54d00d49
Duartetais <tahysduarte22@gmail.com>
2026-03-05 21:46:50 -0300

    Finalizando exercicios de Collections - 05/03

---
+++ atividade0503/atividade3.ts
@@ -0,0 +1,21 @@
+import readlinesync = require("readline-sync");
+
+
+let numeros = new Set();
+
+console.log("Digite 10 números inteiros não repetidos:");
+
+
+while (numeros.size < 5
+
+) {
+    let valor = readlinesync.questionInt("Digite um numero: ");
+    
+   
+    numeros.add(valor);
+}
+
+console.log("Listar dados do Set:");
+numeros.forEach(num => {
+    console.log(num);
+});
